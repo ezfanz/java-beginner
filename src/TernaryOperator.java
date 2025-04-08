8d450e6be8c65aafd00ae55e8398205dc36db12f
irfan <irfanzulkefly144@gmail.com>
2025-04-08 11:43:09 +0800

    feat - added another example

---
+++ src/TernaryOperator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TernaryOperator {
+    public static void main(String[] args) {
+
+        /*
+         * ternary operator ? = Return 1 of 2 values if a condition is true
+         * variable = (condition) ? ifTrue : ifFalse;
+         * */
+
+        int score = 70;
+        int number = 3;
+        int hours = 9;
+        int income = 60000;
+
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+        String timeOfDay = (hours < 12) ? "A.M" : "P.M.";
+
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+
+        System.out.println(passOrFail);
+        System.out.println(evenOrOdd);
+        System.out.println(timeOfDay);
+
+        System.out.println(taxRate);
+
+    }
+}
--- src/WeightConversionProgram.java
+++ src/WeightConversionProgram.java
@@ -20,18 +20,20 @@ public class WeightConversionProgram {
         System.out.print("Choose an option: ");
         choice = scanner.nextInt();
 
-        if(choice == 1){
+        if (choice == 1) {
             System.out.print("Enter the weight in lbs: ");
             weight = scanner.nextDouble();
             newWeight = weight * 0.453592;
             System.out.printf("The new weight in kgs is: %.2f", newWeight);
-        }
-        else if(choice == 2){
+        } else if (choice == 2) {
             System.out.print("Enter the weight in kgs: ");
             weight = scanner.nextDouble();
             newWeight = weight * 2.20462;
             System.out.printf("The new weight in lbs is: %.2f", newWeight);
+        } else {
+            System.out.println("That was not a valid choice");
         }
 
+        scanner.close();
     }
 }
